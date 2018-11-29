035669cc4d6e1cb8de5ae8df684191583165f916
fredlc <fredlefevre@outlook.com>
2018-11-29 13:28:05 -0500

    Created LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Fred Lefevre on 2018-11-29.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Fred Lefevre on 2018-11-29.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
